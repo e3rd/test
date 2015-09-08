563b06db702ce22bc1a54a1116d6ef755104bf51
Edvard Rejthar <edvard.rejthar@nic.cz>
2015-09-08 18:21:09 +0200

    pokus all folders

---
+++ pokus/pokus/src/pokus.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "edvard"
+__date__ = "$Sep 8, 2015 6:08:51 PM$"
+
+if __name__ == "__main__":
+    print "Hello World"
